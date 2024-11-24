ae33b0c8103a4abe643aad65934aa701e0460e8e
mahmouds23an <pgemahmoud@gmail.com>
2024-11-24 22:18:03 +0200

    fix errors in registration process

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,16 +5,18 @@ import generateToken from "../utils/generateToken.js";
 
 const register = async (req, res) => {
   try {
-    const { name, email, password, role } = req.body;
+    const { firstName, lastName, email, password, avatar, role } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({
-      name,
+      firstName,
+      lastName,
       email,
       password: hashedPassword,
+      avatar,
       role,
       wallet: { balance: 0, transactions: [] },
     });
